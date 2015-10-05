a84674da7676c4e1169b5572126cdfc455b98c65
vikashkrkashyap <vikashkrkashyap@gmail.com>
2015-10-06 03:28:33 +0530

    appkey

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomStrings'),
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
