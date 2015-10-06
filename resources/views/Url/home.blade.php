b1f5630237c4a54cf69d6a4be914788c1980bdb7
vikashkrkashyap <vikashkrkashyap@gmail.com>
2015-10-06 06:11:11 +0530

    hompage

--- resources/views/Url/home.blade.php
+++ resources/views/Url/home.blade.php
@@ -1,9 +1,11 @@
-<div style="margin:17% 25%;padding:25px 20px;">
+<div style="margin:5% 25%;padding:25px 20px;">
+    <p style="font-size:50px;color:tomato;margin:15% 35%;font-family:"Helvetica Neue", Helvetica, Arial, sans-serif;">Ucut.in</p>
     <form method="post" action="{{url('show')}}">
         {!! csrf_field() !!}
         <input type="url" name ="input_data"placeholder="Enter Your link" required ="required"
-               style="width:450px;height:50px;border:2px solid teal;font-size:20px;font-weight:700;padding:8px 5px;border-radius:2px; ">
-        <input type="submit" value="Generate Link"
-               style="width:150px;height:50px;margin:5% auto;padding:5px;background:teal;color:white;font-size:18px;font-weight: 600;">
+               style="width:70%;height:50px;border:2px solid tomato;font-size:15px;font-weight:500;padding:8px 5px;border-radius:2px; ">
+        <input type="submit" value="Cut Link"
+               style="width:150px;height:50px;margin:2% auto;padding:5px 3px;background:tomato;color:white;
+               border:2px solid tomato;font-size:15px;font-weight: 600;">
     </form>
 </div>
