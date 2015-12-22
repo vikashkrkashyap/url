58675e0430c49e9d0084ae792bbe8e15696f2051
Ashish Singh <imrealashu@gmail.com>
2015-12-22 20:49:29 +0530

    Improvements

--- app/Http/Controllers/NewUrlController.php
+++ app/Http/Controllers/NewUrlController.php
@@ -73,9 +73,9 @@ class NewUrlController extends Controller
             $current_ip = $request->getClientIp();
             $test = app('Illuminate\Routing\UrlGenerator')->previous();
 
-            // $name = $this->get_title($test);
-            $name = 'google.com';
-            $location = GeoIPFacade::getLocation('202.142.69.126');
+             $name = $this->get_title($test);
+//            $name = 'google.com';
+            $location = GeoIPFacade::getLocation($current_ip);
             $os_info = parser::detect();
 
             //City data
